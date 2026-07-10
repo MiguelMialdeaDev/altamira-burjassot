1fc45e75d2a7e74304a450ee5bd2f52e1f9ee187
Miguel Mialdea <miguelmialdeamonzo@gmail.com>
2026-07-10 15:25:59 +0200

    dominio propio altamiragcp.es: site + base raiz

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://miguelmialdeadev.github.io',
-  base: '/altamira-burjassot',
+  site: 'https://altamiragcp.es',
+  base: '/',
   output: 'static',
   integrations: [tailwind(), sitemap()],
   i18n: {
